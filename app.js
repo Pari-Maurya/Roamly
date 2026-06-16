9497080e72b56855c76e39cd67f8e9c2f8254896
Pari-Maurya <parimaurya2006@gmail.com>
2026-06-16 23:44:06 +0530

    first route created

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 const mongoose = require('mongoose');
+const Listing = require('./models/listing');
 
 async function main(){
   await mongoose.connect('mongodb://127.0.0.1:27017/roamly');
@@ -11,6 +12,17 @@ main().then(() => console.log('Connected to MongoDB')).catch(err => console.log(
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+app.get("/testListing", async (req, res) => {
+  let sampleListing = new Listing({
+    title: "My new Villa",
+    description: "By the beach",
+    price: 1500,
+    location: "Mumbai",
+    country: "India"
+  });
+  await sampleListing.save();
+  res.json(sampleListing);
 });
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
